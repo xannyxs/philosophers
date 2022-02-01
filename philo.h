9f6716f9173024c9f736132cd4c522156d297c2e
Dikkexander <xandervoorvaart@protonmail.com>
2022-02-01 14:32:47 +0100

    First steps of philo

---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/01 14:09:18 by xvoorvaa      #+#    #+#                 */
+/*   Updated: 2022/02/01 14:22:16 by xvoorvaa      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <string.h>
+
+/*
+    PHILO
+*/
+
+#endif
